2619fb89b7ee60fc823a8f4a24f7170a056cc487
damianhouse <damianhouse@gmail.com>
2016-02-19 14:29:55 -0500

    Person B Bullet 11. Create test and code for user photo url to start with http:// or https:// for validation.

--- user.rb
+++ user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-
+  validates_format_of :photo_url, :with => /A(http|https):\/\//, :on => :create
   def full_name
     "#{title + " " if title}#{first_name} #{padded_middle_initial}#{last_name}"
   end
